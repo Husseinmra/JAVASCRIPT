53f73fbd567b3d4ad8cb8899004d3c0091501bd3
HUSSEIN ABDALLAH <Abdallah.suni@gmail.com>
2025-05-26 13:44:30 -0300

    teste de js

---
+++ TESTES/js/script.js
@@ -0,0 +1,4 @@
+const num1 = prompt('Digite o primeiro número')
+const num2 = prompt('Digite o segundo número')
+const resultado = Number (num1) + Number (num2)
+alert (`O resulado das soma dos dois números digitados é ${resultado}`)
--- teste.js
+++
@@ -1,9 +0,0 @@
-const nome = 'Hussein';
-const sobrenome = 'Abdallah';
-const idade = 28;
-const peso = 84;
-const altura = 1.75;
-let imc = peso / (altura * altura);
-let anoNascimento = 2025 - idade;
-
-console.log(`Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e peso ${peso} kg. Minha altura é ${altura} m e meu IMC é ${imc}. Eu nasci em ${anoNascimento}.`);
