b3a716da815b1abd9c0e7e40475d2d976324350e
HUSSEIN ABDALLAH <Abdallah.suni@gmail.com>
2025-06-05 21:23:53 -0300

    exercicio js imc

--- TESTES/js/script.js
+++ TESTES/js/script.js
@@ -1,10 +1,20 @@
-const prompt1 = Number(prompt('Digite um número:'));
-const num = document.getElementById('numero');
-num.innerHTML = prompt1;
 
-document.body.innerHTML += `O numero digitado é inteiro? R: ${Number.isInteger(prompt1)} <br>`;
-document.body.innerHTML += ``
-document.body.innerHTML += ``
-document.body.innerHTML += ``
-document.body.innerHTML += ``
+const form = document.querySelector ('#form');
+const peso = document.querySelector ('#peso');
+const altura = document.querySelector ('#altura');
+const res = document.querySelector ('#resultado');
+function calcularIMC() {
+  const pesonum = Number(peso.value);
+  const alturanum = Number(altura.value);
+  if (pesonum == 0 || alturanum == 0) {
+    window.alert("Preencha os campos com valores válidos.");
+  } else {
+    const imc = pesonum / (alturanum * alturanum);
+    res.innerHTML = `Seu IMC é ${imc}`;
+  }
+    
+}
+addEventListener('submit', function(event) {
+  event.preventDefault(); // Impede o envio do formulário
+});
 
