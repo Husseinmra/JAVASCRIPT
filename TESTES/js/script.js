857b4972cebf7b79a5d68cfe2550fa2a885e229c
HUSSEIN ABDALLAH <Abdallah.suni@gmail.com>
2025-07-17 11:40:29 -0300

    final

--- TESTES/js/script.js
+++ TESTES/js/script.js
@@ -1,63 +1,35 @@
+const tarefa = document.querySelector(".task");
+const botao = document.querySelector(".submit");
+const lista = document.querySelector(".list");
 
-
-
-let conta = null
-let i = 0
-
-function transformaEmHorasMinutosSegundos(segundos) {
-  const horas = Math.floor(segundos / 3600);
-  const minutos = Math.floor((segundos % 3600) / 60);
-  const segundosRestantes = segundos % 60;
-  return `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundosRestantes.toString().padStart(2, '0')}`;
-}
-
-
-function contar() {
-  if (conta !== null) {
-    return;
+function add() {
+  if (!tarefa.value) {
+   tarefa.style.border = "2px solid red";
+   setTimeout(() => {
+    tarefa.style.border = "";
+   }, 1500);
+   return;
+  } 
+  else {
+    tarefa.style.border = "2px solid black";
+    const item = document.createElement("li");
+    item.innerText = tarefa.value;
+    lista.appendChild(item);
+    const erase = document.createElement("button");
+    erase.innerText = "APAGAR";
+    erase.classList.add("erase");
+    item.appendChild(erase);
+    erase.addEventListener("click", function () {
+    lista.removeChild(item);
+    });
   }
-  const tempo = document.querySelector('.container h1')
-  const iniciar = document.querySelector('#iniciar')
-  conta = setInterval ( function () {
-    tempo.innerHTML = transformaEmHorasMinutosSegundos(i);
-    i++;
-  }, 1000)
-  const para = setTimeout(function() {
-    clearInterval(conta)
-  }, 86400000)
-  tempo.style.color = 'black';
-
 }
 
-function parar () {
-  const tempo = document.querySelector('.container h1')
-if (conta != null) {
-  clearInterval(conta);
-}
-  tempo.style.color = 'rgba(245, 55, 55, 0.85';
-}
-
-function zerar() {
-  const tempo = document.querySelector('.container h1');
-  const zerar = document.querySelector('#zerar');
-
-  // Para o contador se estiver rodando
-  if (conta != null) {
-    clearInterval(conta);
-    conta = null;
+tarefa.addEventListener ('keypress', function (x) {
+  if (x.key === "Enter") {
+    x.preventDefault();
+    add();
+    tarefa.value = "";
   }
-
-  // Zera o valor da contagem
-  i = 0;
-
-  // Atualiza o texto
-  tempo.innerHTML = transformaEmHorasMinutosSegundos(i);
-
-  // Opcional: volta cor ao normal
-  tempo.style.color = 'black';
-}
-
-function zeroAEsquerda(num) {
-  return num <= 60 ? num : `0${num}`;
-}
+});
 
